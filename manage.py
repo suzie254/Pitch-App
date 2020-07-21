d7443e4296c3c94e38e5051cff0aa30b878eb65b
suzie254 <suzieakinyi08@gmail.com>
2020-07-21 17:51:25 +0300

    Did the identation

--- manage.py
+++ manage.py
@@ -17,7 +17,6 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 @manager.shell
-
 def make_shell_context():
     return dict(app = app,db = db,User = User, Pitch = Pitch, Comment = Comment ) 
 
